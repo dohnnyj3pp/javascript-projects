cbef9a3f1ba482e5d80686347527fb6822efc0ba
dohnnyj3pp <dohnnypuppet@gmail.com>
2024-12-17 12:18:38 -0700

    pushing my JS assignments

---
+++ Basic Javascripts Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,4 @@
+var Sent1 = "This is the beginning of the string";      //This is the first variable created
+var Sent2 = " and this is the end of the string";       //This is the second variable created
+window.alert(Sent1 + Sent2);                            //This is concatenating the variables in a pop-up window in browser.
+document.write(10 + 5 - 2);                             //This displays written text of the equation for this expression.             
