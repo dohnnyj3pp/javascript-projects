f77d6a30fe903f1f05ecb54e5d9691246a99d168
dohnnyj3pp <dohnnypuppet@gmail.com>
2024-12-17 13:24:52 -0700

    operator assignment

---
+++ Basic Javascripts Projects/project3_math_operators/JS/main.js
@@ -0,0 +1,9 @@
+    <script>
+        function performAddition() {
+            // Perform the addition operation
+            var result = 5 + 3; // Example addition
+            // Update the paragraph element with the result
+            document.getElementById("Math").innerText = "Result: " + result;
+        }
+    </script>
+
